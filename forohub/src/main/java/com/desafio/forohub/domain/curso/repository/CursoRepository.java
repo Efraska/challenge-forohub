0edaacb5198170cdb6d8ba1ec345e3d19aa1b6c0
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2025-01-15 21:10:24 -0600

    Renombrando clase y Creando el CRUD en la clase CursoController

--- forohub/src/main/java/com/desafio/forohub/domain/curso/repository/CursoRepository.java
+++ forohub/src/main/java/com/desafio/forohub/domain/curso/repository/CursoRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepositoryCurso extends JpaRepository<Curso, Long> {
+public interface CursoRepository extends JpaRepository<Curso, Long> {
 
     Page<Curso> findAllByActivoTrue(Pageable pageable);
 }
