7789383587aa6b06a07f09576fe1723f32938da6
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2025-01-16 12:35:08 -0600

    implementando paquete de controladores y domain

--- forohub/src/main/java/com/desafio/forohub/controller/CursoController.java
+++ forohub/src/main/java/com/desafio/forohub/controller/CursoController.java
@@ -8,20 +8,20 @@ import com.desafio.forohub.domain.curso.repository.CursoRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cursos")
 @SecurityRequirement(name = "bearer-key")
-@Tag(name = "Curso", description = "Puede pertenecer a una de las muchas categorias definidas")
+@Tag(name = "Curso", description = "Puede pertenecer a una de las muchas categorías definidas")
 public class CursoController {
 
     @Autowired
@@ -29,33 +29,34 @@ public class CursoController {
 
     @PostMapping
     @Transactional
-    @Operation(summary = "Reistrar un nuevo curso en la BD.")
+    @Operation(summary = "Registrar un nuevo curso en la BD.")
     public ResponseEntity<DetalleCursoDTO> crearTopico(@RequestBody @Valid CrearCursoDTO crearCursoDTO,
-                                                       UriComponentsBuilder uriBuilder) {
+                                                       UriComponentsBuilder uriBuilder){
         Curso curso = new Curso(crearCursoDTO);
         repository.save(curso);
         var uri = uriBuilder.path("/cursos/{i}").buildAndExpand(curso.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new DetalleCursoDTO(curso));
+
     }
 
-    @GetMapping
+    @GetMapping("/all")
     @Operation(summary = "Lee todos los cursos independientemente de su estado")
-    public ResponseEntity<Page<DetalleCursoDTO>> listarCursos(@PageableDefault(size = 5, sort = {"id"})Pageable pageable) {
+    public ResponseEntity<Page<DetalleCursoDTO>> listarCursos(@PageableDefault(size = 5, sort = {"id"}) Pageable pageable){
         var pagina = repository.findAll(pageable).map(DetalleCursoDTO::new);
         return ResponseEntity.ok(pagina);
     }
 
     @GetMapping
     @Operation(summary = "Lista de cursos activos")
-    public ResponseEntity<Page<DetalleCursoDTO>> listarCursosActivos(@PageableDefault(size = 5, sort = {"id"}) Pageable pageable) {
+    public ResponseEntity<Page<DetalleCursoDTO>> listarCursosActivos(@PageableDefault(size = 5, sort = {"id"}) Pageable pageable){
         var pagina = repository.findAllByActivoTrue(pageable).map(DetalleCursoDTO::new);
         return ResponseEntity.ok(pagina);
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Lee un solo curso por su id")
-    public ResponseEntity<DetalleCursoDTO> ListarUnCurso(@PathVariable Long id) {
+    @Operation(summary = "Lee un solo curso por su ID")
+    public ResponseEntity<DetalleCursoDTO> ListarUnCurso(@PathVariable Long id){
         Curso curso = repository.getReferenceById(id);
         var datosDelCurso = new DetalleCursoDTO(
                 curso.getId(),
@@ -69,8 +70,8 @@ public class CursoController {
     @PutMapping("/{id}")
     @Transactional
     @Operation(summary = "Actualiza el nombre, la categoría o el estado de un curso")
-    public ResponseEntity<DetalleCursoDTO> actualizarCurso(@PathVariable Long id,
-                                                              @RequestBody @Valid ActualizarCursoDTO actualizarCursoDTO) {
+    public ResponseEntity<DetalleCursoDTO> actualizarCurso(@RequestBody @Valid ActualizarCursoDTO actualizarCursoDTO, @PathVariable Long id){
+
         Curso curso = repository.getReferenceById(id);
 
         curso.actualizarCurso(actualizarCursoDTO);
@@ -86,10 +87,11 @@ public class CursoController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    @Operation(summary = "Elimina un curso por su id")
-    public ResponseEntity<Void> eliminarCurso(@PathVariable Long id) {
+    @Operation(summary = "Elimina un curso")
+    public ResponseEntity<?> eliminarCurso(@PathVariable Long id){
         Curso curso = repository.getReferenceById(id);
         curso.eliminarCurso();
         return ResponseEntity.noContent().build();
     }
+
 }
